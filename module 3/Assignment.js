8baba90f3c457ea76073d9960045da1ce383fed8
LakshX0 <lakshansandeep7@gmail.com>
2024-10-15 12:30:51 +0530

    added new functions

--- module 3/Assignment.js
+++ module 3/Assignment.js
@@ -7,4 +7,22 @@ function consoleStyler(color, background, fontSize, txt) {
   console.log(message);
   console.log(style);
 }
-consoleStyler("red", "blue", 14, "Inter");
+
+function celebrateStyler(reason) {
+  var fontStyle = "color: tomato; font-size: 50px";
+  if (reason == "birthday") {
+    console.log(`%cHappy birthday`, fontStyle);
+  } else if (reason == "champions") {
+    console.log(`%cCongrats on the title!`, fontStyle);
+  } else console.log(message, style);
+}
+
+consoleStyler("#1d5c63", "#ede6db", "40px", "Congrats!");
+celebrateStyler("birthday");
+
+function styleAndCelebrate() {
+  consoleStyler(color, background, fontSize, txt);
+  celebrateStyler(reason);
+}
+
+styleAndCelebrate("ef7c8e", "fae8e0", "30px", "You made it!", "champions");
