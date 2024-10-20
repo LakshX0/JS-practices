21202821ec909f49a160256972c3b2dcb8b6b3d0
LakshX0 <lakshansandeep7@gmail.com>
2024-10-20 23:55:56 +0530

    updated spreadOperator.js file

--- module 3/spreadOperator.js
+++ module 3/spreadOperator.js
@@ -1,3 +1,10 @@
 let top3 = ["Colocium Museam", "Pound", "Vatican City"];
 
-function showTour() {}
+function showTour(place1, place2, place3) {
+  console.log("first visit", place1);
+  console.log("then you can visit", place2);
+  console.log("Your last destiantion is", place3);
+}
+
+// showTour(top3[0], top3[1], top3[2]);
+showTour(...top3);
